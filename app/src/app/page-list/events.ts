12f6ace2b1f0570692932ff31329464e5edd288e
Lei DING <LDing2@scu.edu>
2019-11-20 15:59:24 -0800

    Angular Initialize

---
+++ app/src/app/page-list/events.ts
@@ -0,0 +1,23 @@
+export const events = [
+    {
+      image: '../../assets/event1.png',
+      title: 'I know a place good at cooking Chinese food. Would you like to come?',
+      description: 'Chinese cuisine is an important part of Chinese culture, which includes cuisine originating from the diverse regions of China, as well as from Chinese people in other parts of the world.',
+      date: 'This Sunday 5:00PM',
+      detailUrl: "['/detail']",
+    },
+    {
+      image: '../../assets/event2.jpg',
+      title: 'Lets have a 3vs3 basketball game',
+      description: 'Shoot 3D hoops, challenge your friends in 3-point contests, and perform slam dunks in one of our many free, online basket games! ',
+      date: 'Next Tuesday 9:30AM',
+      detailUrl: "['/detail']",
+    },
+    {
+      image: '../../assets/event3.jpg',
+      title: 'Find Roommates!',
+      description: 'Make Your Move To Domicilio Apartments Today! Located Near Santa Clara Transit Center. Schedule Your Tour Of Our Units And Visit Our Refined Community Today! Call Us Now. Wi-Fi Available. Spa/Hot Tub. Underground Parking. ',
+      date: 'November 13, 10:30AM',
+      detailUrl: "['/detail']",
+    }
+  ];

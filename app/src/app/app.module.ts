91681c370b617018d28d65d1bf3fe241a9af52e8
Yunfan Zhang <yunfan@xmotors.ai>
2019-12-03 16:54:14 -0800

    Add google nearby search feature

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,8 +31,9 @@ import { PageLoginComponent } from './page-login/page-login.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      { path:'', 
+      { path:'',
         children: [
           { path:'', component: PageMainComponent },
           { path:'post', component: PagePostComponent },
--- app/src/app/page-post/page-post.component.ts
+++ app/src/app/page-post/page-post.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {} from 'googlemaps';
+
+//import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Rx';
+
+
+declare module 'googlemaps';
 
 @Component({
   selector: 'app-page-post',
   templateUrl: './page-post.component.html',
   styleUrls: ['./page-post.component.scss']
 })
-export class PagePostComponent implements OnInit {
 
-  constructor() { }
+export class PagePostComponent implements AfterViewInit {
+    constructor(private http: HttpClient) { }
+    @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
 
-  ngOnInit() {
-  }
+    initLat: number = 37.349693;
+    initLng: number = -121.940329;
 
+    apiKey = 'AIzaSyDynfTkjf4B5VdGrP5VNvEcQGe7BtNa5eY';
+
+    initMap(lat: number, lng: number) {
+        var map: google.maps.Map;
+        var initCoordinate = new google.maps.LatLng(lat, lng);
+        var initMapOptions: google.maps.MapOptions = {
+              center: initCoordinate,
+              zoom: 17,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+        var map = new google.maps.Map(this.gmap.nativeElement, initMapOptions);
+    }
+
+    ngAfterViewInit() {
+        this.initMap(this.initLat, this.initLng);
+        //this.getRequest();
+    }
+
+    //TODO: POST rest API to get coordinates
+    getRequest(place) {
+        let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${this.apiKey}&location=${this.initLat},${this.initLng}&radius=500&name=${place}`;
+        //console.log("get url: ", url)
+        //console.log("google map get request",this.http.get(url));
+
+        //console.log(this.http.get(url).map(res => res.json()).subscribe(val => console.log(val)));
+        //console.log(this.http.get(url).map((res: Response) => res.json()));
+
+        //var jsonResult = this.http.get(url).map((res: Response) => res.json());
+
+        //console.log(this.http.get(url));
+
+        // dummy json result
+        var jsonResult = {
+                            "html_attributions": [],
+                            "results": [
+                                {
+                                    "geometry": {
+                                        "location": {
+                                            "lat": 37.3514167,
+                                            "lng": -121.9421215
+                                        }
+                                    },
+                                    "name": "SCU Parking",
+                                    "vicinity": "1065 Alviso St, Santa Clara"
+                                }
+                                ],
+                            "status": "OK"
+                            }
+        //console.log(jsonResult.results[0].geometry.location)
+        if (jsonResult.status == 'OK') {
+            return [jsonResult.results[0].geometry.location, jsonResult.results[0].name, jsonResult.results[0].vicinity];
+        }
+        else {
+            console.log(jsonResult.status);
+            //TODO: prompt alert window
+        }
+    }
+
+    placeMarker(placeInfo, map) {
+        var coordinates = placeInfo[0];
+        var lat = coordinates.lat;
+        var lng = coordinates.lng;
+        var initCoordinate = new google.maps.LatLng(lat, lng);
+
+        var marker = new google.maps.Marker({
+              position: initCoordinate,
+              map: map,
+              title: placeInfo[1]
+            });
+
+        marker.setMap(map);
+    }
+
+    googleSearch(place) {
+        var placeInfo: any[] = this.getRequest(place);
+        console.log(coordinates);
+        var coordinates = placeInfo[0];
+        var lat = coordinates.lat;
+        var lng = coordinates.lng;
+        var map = this.initMap(lat, lng);
+        this.placeMarker(placeInfo, map);
+    }
 }
